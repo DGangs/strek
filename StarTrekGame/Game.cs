2d7efc9b75cb6900c4ed12270b89100a2fdf92af
David Wygergangs <david.wygergangs@fotf.org>
2018-03-15 13:56:37 -0600

    Add game class with stardate

---
+++ StarTrekGame/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarTrekGame
+{
+    public class Game
+    {
+        private int starDate;
+
+        public Game()
+        {
+            starDate = 2600;
+        }
+
+        public int StarDate()
+        {
+            return starDate;
+        }
+    }
+}
---
+++ StarTrekGameTest/GameTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StarTrekGame;
+
+namespace StarTrekGameTest
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void ThereIsACurrentStarDate()
+        {
+            Game game = new Game();
+            Assert.IsNotNull(game.StarDate());
+        }
+    }
+}
